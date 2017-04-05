0f28e0a2687aa8cc743a2eb3489633da724ef6fe
robhang <robert@kemsolutions.com>
2017-04-05 00:04:42 -0400

    Set the objectives for 2.0 version. Let's do it!

--- resources/components/article-container-intro.vue
+++ resources/components/article-container-intro.vue
@@ -22,15 +22,55 @@
                 {{ $store.state.sub }}
             </p>
 
-            <ul class="collection with-header" v-for="(articles, category) in $store.state.articles">
-                <li class="collection-header"><h4>{{ category }}</h4></li>
-                <router-link v-for="article in articles"
-                             :to="article.slug"
-                             class="collection-item"
-                >
-                    {{ article.title }}
-                </router-link>
+            <br>
+
+            <h4 class="header">Progression status</h4>
+            <div class="progress">
+                <div class="determinate" style="width: 0%"></div>
+            </div>
+
+            <ul class="collection with-header">
+                <li class="collection-header"><h5>What's new in 2.0?</h5></li>
+
+                <li class="collection-item">
+                    Previous / Next shortcut
+                </li>
+                <li class="collection-item">
+                    Infinite categories nesting
+                </li>
+                <li class="collection-item">
+                    Automated slug generation
+                </li>
+                <li class="collection-item">
+                    New article render function (ability to set up custom folder directory inside manifest.js)
+                </li>
+                <li class="collection-item">
+                    Search debounced to 250ms
+                </li>
             </ul>
+
+            <ul class="collection with-header">
+                <li class="collection-header"><h5>Upcoming in 2.x</h5></li>
+
+                <li class="collection-item">
+                    Improved rendered layout (code snippets, readability)
+                </li>
+
+                <li class="collection-item">
+                    Full screen layout
+                </li>
+
+            </ul>
+
+            <!--<ul class="collection with-header" v-for="(articles, category) in $store.state.articles">-->
+                <!--<li class="collection-header"><h4>{{ category }}</h4></li>-->
+                <!--<router-link v-for="article in articles"-->
+                             <!--:to="article.slug"-->
+                             <!--class="collection-item"-->
+                <!--&gt;-->
+                    <!--{{ article.title }}-->
+                <!--</router-link>-->
+            <!--</ul>-->
 
         </div>
     </div>
