dd876f9e958dc7f42be36316625963f570a88931
robhang <robert@kemsolutions.com>
2017-05-07 18:35:59 -0400

    Set up goals for 2.1 and > 2.1

--- resources/components/articles/article-container-intro.vue
+++ resources/components/articles/article-container-intro.vue
@@ -24,15 +24,63 @@
 
             <br>
 
+            <h4 class="header">Progression status</h4>
+            <div class="progress">
+                <div class="determinate" style="width: 0%"></div>
+            </div>
+
             <ul class="collection with-header">
-                <li class="collection-header"><h4>All articles</h4></li>
-                <router-link v-for="article in this.$store.state.searchArticles"
-                             :to="article.slug"
-                             class="collection-item"
-                >
-                    {{ article.title }}
-                </router-link>
+                <li class="collection-header"><h5>What's new in 2.1?</h5></li>
+
+                <li class="collection-item">
+                    <i class="tiny material-icons green darken-1" style="color: #fff;">done</i>
+                    Improve code snippets
+                </li>
+
+                <li class="collection-item">
+                    Improve readability (Github style?)
+                </li>
+
+                <li class="collection-item">
+                    Allow custom css styling
+                </li>
+
+                <li class="collection-item">
+                    Full screen layout - different sidebar width
+                </li>
+
             </ul>
+
+            <ul class="collection with-header">
+                <li class="collection-header"><h5>Upcoming in > 2.1</h5></li>
+
+                <li class="collection-item">
+                    Improve introduction page (list all nested articles)
+                </li>
+
+                <li class="collection-item">
+                    Previous / Next shortcut
+                </li>
+
+                <li class="collection-item">
+                    Keyboard previous / next shortcut
+                </li>
+
+                <li class="collection-item">
+                    Localize hard coded string
+                </li>
+
+            </ul>
+
+            <!--<ul class="collection with-header">-->
+                <!--<li class="collection-header"><h4>All articles</h4></li>-->
+                <!--<router-link v-for="article in this.$store.state.searchArticles"-->
+                             <!--:to="article.slug"-->
+                             <!--class="collection-item"-->
+                <!--&gt;-->
+                    <!--{{ article.title }}-->
+                <!--</router-link>-->
+            <!--</ul>-->
 
         </div>
     </div>
