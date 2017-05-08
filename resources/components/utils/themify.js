5751a2c1e885aa0f10902c4a184565214981d9f5
robhang <robert@kemsolutions.com>
2017-05-08 00:11:33 -0400

    Added comments.

--- resources/components/utils/themify.js
+++ resources/components/utils/themify.js
@@ -1,7 +1,27 @@
 const options = require('../../../manifest').options;
 
+/**
+ * This module is responsible for everything UI related.
+ *
+ */
 export default {
+
+    /**
+     * Entry point.
+     *
+     * @returns {*}
+     */
     mounted() {
+        this.__setTheme()
+    },
+
+    /**
+     * Set the appropriate theme.
+     *
+     * @returns {*}
+     * @private
+     */
+    __setTheme() {
         switch (options.theme) {
             case 'default':
                 return null;
