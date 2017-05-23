58b402193279376564d03a7ee79ecfcd0a84281c
robhang <robert@kemsolutions.com>
2017-05-22 21:27:53 -0400

    Updated webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-    entry: './resources/main.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist/',
