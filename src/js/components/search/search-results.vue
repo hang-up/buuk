9d68d8da4627b4558aecd3e5f29549bb4bde2fb3
robhang <robert@kemsolutions.com>
2017-05-25 22:49:42 -0400

    Styled search results.

--- src/js/components/search/search-results.vue
+++ src/js/components/search/search-results.vue
@@ -1,3 +1,14 @@
+<style scoped>
+    .collection {
+        position: absolute;
+        width: 100%;
+        z-index: 2;
+        margin-top: 0;
+        border: none;
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+    }
+</style>
+
 <template>
     <div class="collection" v-if="$store.state.searchResults.length && $store.state.query">
         <router-link :to="article.slug"
