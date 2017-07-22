2b91371d0350f205e96bddb66ca1deef87f01a30
robhang <robert@kemsolutions.com>
2017-07-22 12:48:43 -0400

    Updated store to only make use of import statements.

---
+++ src/js/store/modules/bootstrap/index.js
@@ -0,0 +1,20 @@
+import manifest from '../../../../../manifest.json'
+import locale from './locale'
+
+// Get the logo if set inside manifest options.
+const logo = manifest.options.logo ? manifest.options.logo : ""
+
+// Get the appropriate locale from locale file.
+const localizations = locale[manifest.options.lang]
+
+const state = {
+    short_name: manifest.short_name,
+    name: manifest.name,
+    sub: manifest.sub,
+    logo,
+    localizations
+}
+
+export default {
+    state
+}
---
+++ src/js/store/modules/bootstrap/locale.js
@@ -0,0 +1,22 @@
+const en = {
+    'all_articles': "All articles",
+    'toggle_fs': "Toggle fullscreen",
+    'search': "Search...",
+    'no_result': 'No result found',
+    'introduction': "Introduction",
+    'print': "Print page"
+}
+
+const fr = {
+    'all_articles': "Tous les articles",
+    'toggle_fs': "Plein écran",
+    'search': "Rechercher...",
+    'no_result': "Aucun résultat",
+    'introduction': "Introduction",
+    'print': "Imprimer la page"
+}
+
+export default {
+    en,
+    fr
+}
