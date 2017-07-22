175e4edbeb1e47d57c75f3a4588b9968d632befe
robhang <robert@kemsolutions.com>
2017-07-22 12:52:26 -0400

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import Vue from 'vue'
+import App from './App'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import bootstrap from './js/bootstrap/autoload'
+import './sass/app.scss'
 
-Vue.config.productionTip = false;
+// Load the chosen theme.
+require(`./sass/themes/${bootstrap.theme}`)
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+const router = new VueRouter({
+    routes: bootstrap.routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
+    store,
+    router,
+    render: h => h(App)
 });
