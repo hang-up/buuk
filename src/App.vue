aea8eb50d27a177880ef9fa5df373151b0a6c637
robhang <robert@kemsolutions.com>
2017-07-22 12:13:42 -0400

    Updated App shell.

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,79 @@
+<style scoped>
+    main, #nav-mobile {
+        transition: all .3s ease-in;
+    }
+
+    @media screen and (min-width: 992px) {
+        main {
+            margin-left: 300px;
+        }
+    }
+</style>
+
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
-  </div>
+    <div>
+        <!-- Menu. -->
+        <sidebar></sidebar>
+
+        <main>
+
+            <!-- Navigation -->
+            <navigation></navigation>
+
+            <div class="container">
+                <!-- Floating action button -->
+                <fab :is-full-screen.sync="isFullScreen"></fab>
+
+                <!-- Mobile menu trigger. -->
+                <a href="#"
+                   data-activates="nav-mobile"
+                   class="top-nav waves-effect waves-light hide-on-large-only btn button-collapse"
+                   id="nav-mobile-trigger"
+                   style="margin-top: 19.25px">
+                    <i class="material-icons left">menu</i>
+                    Menu
+                </a>
+
+                <!-- Article. -->
+                <router-view></router-view>
+            </div>
+        </main>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+    import fab from './js/components/articles/fab.vue'
+    import sidebar from './js/components/sidebar/sidebar.vue'
+    import articleContainer from './js/components/articles/article-container.vue'
+    import navigation from './js/components/navigation/navigation.vue'
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                isFullScreen: false
+            }
+        },
+        components: {
+            fab,
+            sidebar,
+            articleContainer,
+            navigation
+        },
+        mounted() {
+            /**
+             * Initialize mobile menu trigger.
+             *
+             */
+            $(".button-collapse").sideNav({
+                draggable: true
+            })
+            /**
+             * Initialize tooltip.
+             *
+             */
+            $('.tooltipped').tooltip({delay: 50});
+        }
+    }
 </script>
-
-<style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
-</style>
