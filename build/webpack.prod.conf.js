3aea278bbb773d37c82fdb62c9c8d32ff0dc7710
robhang <robert@kemsolutions.com>
2017-07-30 10:52:51 -0400

    Customized SW prefix.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -102,7 +102,7 @@ var webpackConfig = merge(baseWebpackConfig, {
         ]),
         // service worker caching
         new SWPrecacheWebpackPlugin({
-            cacheId: 'my-vue-app',
+            cacheId: 'buuk-pwa',
             filename: 'service-worker.js',
             staticFileGlobs: ['dist/**/*.{js,html,css,png}'],
             runtimeCaching: [{
