0f90a0ae19a57bf03dafc86d90fca57368339c84
robhang <robert@kemsolutions.com>
2017-12-22 22:31:37 +0100

    Rough draft of interactive CLI.

--- cli/index.js
+++ cli/index.js
@@ -5,11 +5,40 @@ const commander = require('commander')
 const shell = require('shelljs')
 const chalk = require('chalk')
 const cp = require('cp-file').sync
+const jsonfile = require('jsonfile')
+const fs = require('fs')
+
+const inquirer = require('inquirer')
+
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Project name',
+        default: 'Buuk'
+    },
+
+    {
+        type: 'input',
+        name: 'short_name',
+        message: 'Project short name: fewer than 12 characters to not be truncated on homescreens',
+        default: 'Buuk'
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description',
+        default: 'A Buuk project.'
+    }
+]
+
+
 
 // App.
 commander
     .version('0.0.1')
-    .description('Documentation generator')
+    .description('Buuk CLI')
 
 // Init
 commander
@@ -18,17 +47,51 @@ commander
     .description('Initialize buuk entry and output folders')
     .action(folder => {
         try {
-            // TODO: user input for name of the app and check if folder already exists
 
-            // Create destination folder
-            shell.mkdir('-p', folder)
-            shell.cd(folder)
-            shell.mkdir('-p', 'docs')
-            shell.cd('..')
+            if (fs.existsSync(folder)) {
 
-            // Templated init folder.
-            cp(`${__dirname}/manifest.json`, `${shell.pwd().stdout}/${folder}/manifest.json`)
-            console.log(chalk.green('Buuk initialized!'))
+                inquirer.prompt([
+                    {
+                        type: 'confirm',
+                        name: 'folderDoesExist',
+                        message: 'The folder you have specified already exists. Override it?',
+                        default: false
+                    }
+                ]).then(a => {
+                    if (!a.folderDoesExist) process.exit(1)
+                    else {
+                        inquirer.prompt(questions).then(answers => {
+
+                            // Create destination folder.
+                            shell.mkdir('-p', folder)
+                            shell.cd(folder)
+                            shell.mkdir('-p', 'docs')
+                            shell.cd('..')
+
+                            // Write manifest file.
+                            jsonfile.writeFileSync(`${__dirname}/manifest.json`, answers, {spaces: 2, EOL: '\r\n'})
+                            cp(`${__dirname}/manifest.json`, `${shell.pwd().stdout}/${folder}/manifest.json`)
+                            console.log(chalk.green('Buuk initialized!'))
+                        });
+                    }
+                }).catch(e => console.log(e));
+            }
+            else {
+                inquirer.prompt(questions).then(answers => {
+
+                    // Create destination folder.
+                    shell.mkdir('-p', folder)
+                    shell.cd(folder)
+                    shell.mkdir('-p', 'docs')
+                    shell.cd('..')
+
+                    // Write manifest file.
+                    jsonfile.writeFileSync(`${__dirname}/manifest.json`, answers, {spaces: 2, EOL: '\r\n'})
+                    cp(`${__dirname}/manifest.json`, `${shell.pwd().stdout}/${folder}/manifest.json`)
+                    console.log(chalk.green('Buuk initialized!'))
+                })
+                    .catch(e => console.log(e));
+            }
         }
         catch(e) {
             console.log(chalk.red('Error when trying to initialize Buuk. See log below:'))
