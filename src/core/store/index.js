9954c7748eb147dc23a693f470f7b0d424d3d9d4
robhang <robert@kemsolutions.com>
2017-12-23 00:24:56 +0100

    Minor store import.

--- src/core/store/index.js
+++ src/core/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+
+import articles from './modules/articles'
+import search from './modules/search'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-    state: {
-        count: 0
-    },
-    mutations: {
-        increment (state) {
-            state.count++
-        }
+    modules: {
+        articles,
+        search
     }
 })
 
