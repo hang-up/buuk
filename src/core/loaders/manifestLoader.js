3ad4cb475a3aeb780a226c44441f624ac6a74257
robhang <robert@kemsolutions.com>
2017-12-25 15:47:17 +0100

    One level deep manifest recursion done.

--- src/core/loaders/manifestLoader.js
+++ src/core/loaders/manifestLoader.js
@@ -2,19 +2,17 @@ import { articles } from './tmp.json'
 import { store } from '../store/index'
 import ArticlePrimitive from './ArticlePrimitive'
 
-function manifestLoader() {
-
-    let copy = articles
+function manifestLoader(rootArticles = articles) {
 
     /*
         1: import manifest
         2: search through manifest
      */
 
-    Object.values(copy).forEach( topLevel => {
+    Object.values(rootArticles).forEach( topLevel => {
         // If we have objects, aka sub categories within this toplevel
         if (!Array.isArray(topLevel)) {
-            // console.log(topLevel)
+            manifestLoader(topLevel)
         }
         else {
             // If it's an array, no sub cat and we can proceed with primitiving the articles.
