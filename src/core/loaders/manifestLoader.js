aad151777e842400559d71f773fbd4fd479d526c
robhang <robert@kemsolutions.com>
2017-12-25 19:21:25 +0100

    Manifest Loader done. We can now have infinite sub categories with articles attached to intermediate categories!

--- src/core/loaders/manifestLoader.js
+++ src/core/loaders/manifestLoader.js
@@ -2,22 +2,40 @@ import { articles } from './tmp.json'
 import { store } from '../store/index'
 import ArticlePrimitive from './ArticlePrimitive'
 
+/**
+ * Loader to bridge the manifest and the articles.
+ *
+ * @param rootArticles
+ */
 function manifestLoader(rootArticles = articles) {
 
-    /*
-        1: import manifest
-        2: search through manifest
-     */
-
     Object.values(rootArticles).forEach( topLevel => {
-        // If we have objects, aka sub categories within this toplevel
+
+        /*
+            If we have objects (aka sub categories within this level) we recursively load them until
+            we end up with an array. An array is the sign that we have POTENTIALLY reached the deepest
+            level of sub categories.
+          */
         if (!Array.isArray(topLevel)) {
             manifestLoader(topLevel)
         }
         else {
-            // If it's an array, no sub cat and we can proceed with primitiving the articles.
-            topLevel.forEach((article, category) => {
-                article.primitive = new ArticlePrimitive(article.title).computeValue()
+            /*
+                If we get an array, we MIGHT be at the deepest level of sub categories. This is not a certainty
+                since we can have an infinite number of sub categories.
+              */
+            Object.values(topLevel).forEach((article, category) => {
+
+                /*
+                    IMPORTANT:
+                    "title" is a reserved keyword. It is how we know if we have further nested categories.
+                  */
+                if (!article.title) {
+                    manifestLoader(article)
+                }
+                else {
+                    article.primitive = new ArticlePrimitive(article.title).value
+                }
             })
         }
     })
