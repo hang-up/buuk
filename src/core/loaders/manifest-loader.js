9b857dcd2a6855a15391950265e0a02f9c52015e
robhang <robert@kemsolutions.com>
2017-12-26 16:26:35 +0100

    Improved manifest-loader to reflect changes introduced in article primitive.

--- src/core/loaders/manifest-loader.js
+++ src/core/loaders/manifest-loader.js
@@ -34,7 +34,7 @@ function manifestLoader(rootArticles = articles) {
                     manifestLoader(article)
                 }
                 else {
-                    article.primitive = new ArticlePrimitive(article.title).value
+                    article.primitive = new ArticlePrimitive(article.title, article.tags ? article.tags : null).value
                 }
             })
         }
