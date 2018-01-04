6e47cc777877b64d3dea3155cf22973c78109dbf
robhang <robert@kemsolutions.com>
2018-01-04 17:26:06 -0500

    Search feature done!

--- src/core/store/modules/search/index.js
+++ src/core/store/modules/search/index.js
@@ -1,14 +1,45 @@
+import Fuse from 'fuse.js'
+
+const fuseOptions = {
+    keys: [
+        {
+            name: 'article.primitive.tags',
+            weight: 0.3
+        },
+        {
+            name: 'article.primitive.summary',
+            weight: 0.3
+        },
+        {
+            name: 'article.primitive.content',
+            weight: 0.4
+        }
+    ]
+}
 
 const state = {
-
+    flatArticles: [],
+    results: []
 }
 
 const actions = {
+    search({ commit, state }, { query }) {
+        let fuse = new Fuse(state.flatArticles, fuseOptions)
 
+        commit('setResults', {
+            results: fuse.search(query)
+        })
+    }
 }
 
 const mutations = {
+    setResults(state, results) {
+        state.results = results
+    },
 
+    setFlatArticle(state, article) {
+        state.flatArticles.push(article)
+    }
 }
 
 export default {
