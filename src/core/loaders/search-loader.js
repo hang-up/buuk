5606522d2e610b82b47b687d84a58b06aea2fc10
robhang <robert@kemsolutions.com>
2018-01-04 17:25:34 -0500

    Created a search loader to load a flat array of all articles available.

---
+++ src/core/loaders/search-loader.js
@@ -0,0 +1,41 @@
+import { store } from '../store/index'
+
+
+function searchLoader(rootArticles = store.state.core.articles) {
+
+    Object.values(rootArticles).forEach( topLevel => {
+
+        /*
+            If we have objects (aka sub categories within this level) we recursively load them until
+            we end up with an array. An array is the sign that we have POTENTIALLY reached the deepest
+            level of sub categories.
+          */
+        if (!Array.isArray(topLevel)) {
+            searchLoader(topLevel)
+        }
+        else {
+            /*
+                If we get an array, we MIGHT be at the deepest level of sub categories. This is not a certainty
+                since we can have an infinite number of sub categories.
+              */
+            Object.values(topLevel).forEach((article, category) => {
+
+                /*
+                    IMPORTANT:
+                    "title" is a reserved keyword. It is how we know if we have further nested categories.
+                  */
+                if (!article.title) {
+                    searchLoader(article)
+                }
+                else {
+                    store.commit({
+                        type: 'search/setFlatArticle',
+                        article: article
+                    })
+                }
+            })
+        }
+    })
+}
+
+export default searchLoader
