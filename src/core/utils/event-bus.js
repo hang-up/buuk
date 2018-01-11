38a81a366c1b36b2bb80c6a1d2e103804561cb58
robhang <robert@kemsolutions.com>
2018-01-11 17:39:38 -0500

    Made event bus globally available.

--- src/core/utils/event-bus.js
+++ src/core/utils/event-bus.js
@@ -1,2 +1,8 @@
 import Vue from 'vue'
-export const EventBus = new Vue()
+
+/*
+    This Event bus will be used for pub/sub events that fall out of the scope of our vuex store,
+    aka simple communication within two components, notify of configuration loading, ...
+ */
+window.EventBus = new Vue();
+export const EventBus = window.EventBus
