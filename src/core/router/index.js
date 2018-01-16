0f3fc17d7d4edf8bbb63806d176a1cca7820e196
robhang <robert@kemsolutions.com>
2018-01-16 13:29:02 -0500

    Lazyloaded components.

--- src/core/router/index.js
+++ src/core/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import pages from '@/components/pages/pages.vue'
-import intro from '@/components/pages/templates/intro'
+const pages = () => import('@/components/pages/pages')
+const home = () => import('@/components/pages/templates/home')
 
 Vue.use(Router)
 
@@ -10,7 +10,7 @@ const router =  new Router({
         {
             path: '/',
             name: 'home',
-            component: intro
+            component: home
         },
         {
             path: '/articles/:article',
