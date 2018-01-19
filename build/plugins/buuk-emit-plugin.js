762ba192408ad55343c0c14f0a026a385f6ff5ec
robhang <robert@kemsolutions.com>
2018-01-19 12:45:14 -0500

    Created a plugin to move /dist to the right context folder.

---
+++ build/plugins/buuk-emit-plugin.js
@@ -0,0 +1,29 @@
+var ncp = require('ncp').ncp
+var chalk = require('chalk')
+
+function BuukEmitPlugin() {
+
+}
+
+/**
+ * Plugin to copy compiled /dist under the context folder.
+ *
+ * @param compiler
+ */
+BuukEmitPlugin.prototype.apply = function (compiler) {
+    compiler.plugin("done", function (stats) {
+        // Copy /dist from stats.outputPath to stats.context
+        ncp(compiler.outputPath, `${compiler.context}/dist`, function(e) {
+            if (e) {
+                return console.error(e)
+            }
+            console.log(chalk.cyan('  Build complete.\n'))
+            console.log(chalk.yellow(
+                '  Tip: built files are under /dist and meant to be served over an HTTP server.\n' +
+                '  Opening index.html over file:// won\'t work.\n'
+            ))
+        })
+    })
+}
+
+module.exports = BuukEmitPlugin
