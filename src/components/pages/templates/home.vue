9188b05a7af3012844906265eb866586035808bf
robhang <robert@kemsolutions.com>
2018-01-20 16:34:00 -0500

    Scaffolded quick homepage.

--- src/components/pages/templates/home.vue
+++ src/components/pages/templates/home.vue
@@ -1,9 +1,37 @@
 <template>
-    <v-container fluid :class="activeClass" class="pages-container">
-        <v-layout column align-center>
-            <h1 class="title">
-                First, solve the problem. Then, write the code.
+    <v-container fluid :class="activeClass" class="pages-container wiki">
+        <v-layout column>
+            <h1>
+                {{ $store.state.core.config.name }}
             </h1>
+            <h2>
+                {{ $store.state.core.config.short_name }}
+            </h2>
+            <p>
+                {{ $store.state.core.config.description }}
+            </p>
+
+            <hr>
+        </v-layout>
+
+        <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-toolbar dark>
+                        <v-toolbar-title>All articles</v-toolbar-title>
+                    </v-toolbar>
+                    <v-list>
+                        <v-list-tile v-for="item in $store.state.search.flatArticles"
+                                     v-bind:key="item.article.slug" @click="$router.push({ name:'article', params: { article: item.article.slug } })">
+                            <v-list-tile-content>
+                                <v-list-tile-title v-text="item.article.primitive.name"></v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </v-list>
+                </v-card>
+            </v-flex>
+        </v-layout>
+
         </v-layout>
     </v-container>
 </template>
